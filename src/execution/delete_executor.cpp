1f09047208b22f190e7ca2f37c167e6e590a200c
HonestDeng <2958906959@qq.com>
2024-02-28 12:19:29 +0800

    完成Delete

--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -18,10 +18,36 @@ namespace bustub {
 
 DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), child_executor_(std::move(child_executor)) {}
 
-void DeleteExecutor::Init() { throw NotImplementedException("DeleteExecutor is not implemented"); }
+void DeleteExecutor::Init() { child_executor_->Init(); }
 
-auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+  Tuple child_tuple{};
+  auto catalog = exec_ctx_->GetCatalog();
+  auto table_info = catalog->GetTable(plan_->table_oid_);
+  auto indexes_info = catalog->GetTableIndexes(table_info->name_);
+  int cnt = 0;
+
+  while (true) {
+    const auto status = child_executor_->Next(&child_tuple, rid);
+    if (!status) {
+      // 返回实际插入的tuple的条数
+      Value cnt1(INTEGER, cnt);
+      std::vector<Value> values;
+      values.push_back(cnt1);
+      *tuple = Tuple(values, &GetOutputSchema());
+      return true;
+    }
+
+    // 删除tuple和索引
+    auto meta = table_info->table_->GetTupleMeta(*rid);
+    meta.is_deleted_ = true;
+    table_info->table_->UpdateTupleMeta(meta, *rid);
+    for (auto &index_info : indexes_info) {
+      index_info->index_->DeleteEntry(child_tuple, *rid, nullptr);
+    }
+  }
+}
 
 }  // namespace bustub
